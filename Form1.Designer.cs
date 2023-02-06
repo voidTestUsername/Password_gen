0b7ff9a2fc8ee58eb6b6d49fcf5cfcc5d20e4848
voidTestUsername <skypes306@gmail.com>
2023-02-06 22:29:18 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Password_gen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(278, 123);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(245, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать пароль";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Добро пожаловать! ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(352, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Для начала работы нажмите на кнопку ниже ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.Location = new System.Drawing.Point(287, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Скопировать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(702, 293);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Генератор паролей";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_gen
+{
+    public partial class Form1 : Form
+    {
+        class Symbol_set
+        {
+            public int val, border1, border2;
+            public void set_val(Random rand, int n1, int n2)
+            {
+                val = rand.Next(n1, n2);
+            }
+            public void set_borders(string setting)
+            {
+                if (setting == "A")
+                {
+                    border1 = 65;
+                    border2 = 90;
+                }
+                else if(setting == "a")
+                {
+                    border1 = 97;
+                    border2 = 122;
+                }
+                else if(setting == "0_")
+                {
+                    border1 = 48;
+                    border2 = 57;
+                }
+                else if(setting == "!")
+                {
+                    border1 = 33;
+                    border2 = 42;
+                }
+            }
+            public Symbol_set()
+            {
+                val = 0;
+                border1 = 0;
+                border2 = 0;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Symbol_set set1 = new Symbol_set();
+            Symbol_set set2 = new Symbol_set();
+            Symbol_set set3 = new Symbol_set();
+            Symbol_set set4 = new Symbol_set();
+
+            int set_L = 4;
+            Symbol_set[] set_arr = new Symbol_set[set_L];
+
+            Random rand = new Random();
+            int rand_key;
+
+            const int L = 8;
+            char[] password = new char[L];
+            int[] pos_s = new int[L];
+
+            set1.set_borders("A");
+            set2.set_borders("a");
+            set3.set_borders("0_");
+            set4.set_borders("!");
+
+            rand_key = rand.Next(0, 100);
+
+            if (rand_key < 50)
+            {
+                set1.val = 2;
+
+                rand_key = rand.Next(0, 100);
+
+                if (rand_key < 50)
+                {
+                    set2.val = 2;
+
+                    rand_key = rand.Next(0, 100);
+
+                    if(rand_key < 50)
+                    {
+                        set3.val = 2;
+                        set4.val = 2;
+                    }
+                    else
+                    {
+                        set3.val = 3;
+                        set4.val = 1;
+                    }
+                }
+                else
+                {
+                    set2.val = 3;
+
+                    rand_key = rand.Next(0, 100);
+
+                    if (rand_key < 50)
+                    {
+                        set3.val = 1;
+                        set4.val = 2;
+                    }
+                    else
+                    {
+                        set3.val = 2;
+                        set4.val = 1;
+                    }
+                }
+            }
+            else
+            {
+                set1.val = 3;
+                set2.val = 2;
+
+                rand_key = rand.Next(0, 100);
+
+                if (rand_key < 50)
+                {
+                    set3.val = 1;
+                    set4.val = 2;
+                }
+                else
+                {
+                    set3.val = 2;
+                    set4.val = 1;
+                }
+            }
+
+            set_arr[0] = set1;
+            set_arr[1] = set2;
+            set_arr[2] = set3;
+            set_arr[3] = set4;
+
+            for (int i = 0; i < L; i++)
+            {
+                pos_s[i] = i;
+            }
+
+            int n1, n2, temp_, set_c = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                n1 = rand.Next(0, L / 2);
+                n2 = rand.Next(L / 2, L);
+
+                temp_ = pos_s[n1];
+                pos_s[n1] = pos_s[n2];
+                pos_s[n2] = temp_;
+            }
+
+            for (int i = 0; i < L; i++)
+            {
+                if (set_arr[set_c].val == 0)
+                {
+                    set_c += 1;
+                }
+
+                password[pos_s[i]] = (char)(rand.Next(set_arr[set_c].border1, set_arr[set_c].border2));
+
+                set_arr[set_c].val -= 1;
+            }
+
+            textBox1.Clear();
+
+            for (int i = 0; i < L; i++)
+            {
+                textBox1.Text += password[i];
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+    }
+}
